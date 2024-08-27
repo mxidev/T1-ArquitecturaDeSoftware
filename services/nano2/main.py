93a1a774158e5c328c57cc5bad46424f77a3894e
mxidev <mazipantuflin@gmail.com>
2024-08-26 23:52:17 -0400

    Add main files

---
+++ services/nano2/main.py
@@ -0,0 +1,21 @@
+import logging
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Configurar el logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@app.get("/")
+def read_root():
+    logger.info("Root endpoint accessed")
+    return {"message": "Hello from (nano)Service 2"}
+
+
+@app.get("/health")
+def health_check():
+    logger.info("Health check endpoint accessed")
+    return {"status": "ok"}
